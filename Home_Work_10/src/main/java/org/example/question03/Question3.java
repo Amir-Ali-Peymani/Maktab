75e6d5ebfa3248ee8af70265e0936e00e062a5d6
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 05:20:26 +0330

    change for Main to get called

--- Home_Work_10/src/main/java/org/example/question03/Question3.java
+++ Home_Work_10/src/main/java/org/example/question03/Question3.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Question3 {
-    public static void main(String[] args) {
+    public static void start() {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(3);
         list.add(4);
@@ -18,22 +18,28 @@ public class Question3 {
 //        list.add(10);
         getValue(list);
     }
+
     private static void getValue(ArrayList<Integer> list01){
         if (list01.size() == 0) {
             System.out.println(list01);
             System.out.println("empty");
             return;
         }
-        System.out.println(list01);
+
         if (list01.size() % 2 != 0) {
             int remove =  list01.size() - 1;
             list01.remove(remove);
         }
-        System.out.println(list01);
+
+
         for (int i = 0; i < list01.size() - 1; i+=2) {
             System.out.print("{"+list01.get(i)+", "+list01.get(i+1)+"} ,");
         }
+
         System.out.println();
+        print(list01);
+    }
+    private static void print(ArrayList<Integer> list01){
         ArrayList<Integer> inAppropriate = new ArrayList<>();
         ArrayList<Integer> appropriate = new ArrayList<>();
         for (int i = 0; i < list01.size() - 1; i+=2) {
@@ -52,15 +58,12 @@ public class Question3 {
         }
         System.out.println("-----------------------------------------------------------------");
         System.out.println(list01);
-        System.out.println("inappropriate: "+inAppropriate);
-        System.out.println("appropriate: "+appropriate);
         list01.clear();
         list01.addAll(appropriate);
         System.out.println("after edition: "+list01);
-        System.out.println("THE LIST:");
+        System.out.println("THE CANDIDATES ARE: ");
         for (int i = 0; i < list01.size() - 1; i+=2) {
             System.out.print("{"+list01.get(i)+", "+list01.get(i+1)+"} ");
         }
-
     }
 }
