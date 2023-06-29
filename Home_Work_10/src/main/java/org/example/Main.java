a69785dab6e95cc32e50981ff1cf91bfee9d366d
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 09:09:50 +0330

    delete the imports of previous

--- Home_Work_10/src/main/java/org/example/Main.java
+++ Home_Work_10/src/main/java/org/example/Main.java
@@ -1,14 +1,5 @@
 package org.example;
 
-import org.example.exception.CustomException;
-import org.example.question01.Question1_A;
-import org.example.question01.Question1_B;
-import org.example.question02.Question2_A;
-import org.example.question03.Question3;
-import org.example.question04.MyHashMap;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
