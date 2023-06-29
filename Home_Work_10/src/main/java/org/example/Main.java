45af93465c9e8638a09b5382f4da181776633ddf
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 05:19:23 +0330

    testing questions my calling them in main

--- Home_Work_10/src/main/java/org/example/Main.java
+++ Home_Work_10/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.question01.Question1_A;
+import org.example.question01.Question1_B;
+import org.example.question02.Question2_A;
+import org.example.question03.Question3;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Question3.start();
+//        Question2_A.start();
+//        Question1_B.start();
+//        Question1_A.start();
     }
 }
