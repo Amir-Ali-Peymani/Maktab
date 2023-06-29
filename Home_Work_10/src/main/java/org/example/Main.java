9616f835bdb48dab0049a054f40996cdb4a342e5
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 08:53:22 +0330

    making main ready

--- Home_Work_10/src/main/java/org/example/Main.java
+++ Home_Work_10/src/main/java/org/example/Main.java
@@ -3,14 +3,29 @@ package org.example;
 import org.example.exception.CustomException;
 import org.example.question01.Question1_A;
 import org.example.question01.Question1_B;
+import org.example.question02.Question2_A;
+import org.example.question03.Question3;
+import org.example.question04.MyHashMap;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CustomException customException = new CustomException();
+//        Question1_A.start();
+//        CustomException customException = new CustomException();
 //        Question3.start();
 //        Question2_A.start();
 //        Question1_B.start();
 //        Question1_A.start(customException.giveInput("input"));
 //        Question1_B.start();
+//        MyHashMap<String , Integer> hashMap = new MyHashMap<>();
+//        hashMap.put("1", 1);
+//        hashMap.containsKey("1");
+//        hashMap.get("1");
+//        hashMap.isEmpty();
+//        hashMap.getAll();
+//        hashMap.remove("1");
+//        hashMap.change("1", 2);
+
     }
 }
