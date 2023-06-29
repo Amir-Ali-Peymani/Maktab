27e0f2cdc94b20eec744b8090cd8e096e40ecd32
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 09:08:26 +0330

    end

--- Home_Work_10/src/main/java/org/example/Main.java
+++ Home_Work_10/src/main/java/org/example/Main.java
@@ -11,21 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Question1_A.start();
-//        CustomException customException = new CustomException();
-//        Question3.start();
-//        Question2_A.start();
-//        Question1_B.start();
-//        Question1_A.start(customException.giveInput("input"));
-//        Question1_B.start();
-//        MyHashMap<String , Integer> hashMap = new MyHashMap<>();
-//        hashMap.put("1", 1);
-//        hashMap.containsKey("1");
-//        hashMap.get("1");
-//        hashMap.isEmpty();
-//        hashMap.getAll();
-//        hashMap.remove("1");
-//        hashMap.change("1", 2);
+
 
     }
 }
