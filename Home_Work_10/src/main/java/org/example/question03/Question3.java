b3237f985fdd813840ee40b190732c10c27c74c7
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 07:15:21 +0330

    added methods and check different values for making sure

--- Home_Work_10/src/main/java/org/example/question03/Question3.java
+++ Home_Work_10/src/main/java/org/example/question03/Question3.java
@@ -1,53 +1,55 @@
 package org.example.question03;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Question3 {
     public static void start() {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(3);
         list.add(4);
+        list.add(3);
         list.add(8);
-        list.add(2);
+        list.add(8);
         list.add(9);
         list.add(5);
         list.add(6);
         list.add(7);
         list.add(9);
-//        list.add(10);
-        getValue(list);
+        list.add(10);
+        getChecking(list);
     }
 
-    private static void getValue(ArrayList<Integer> list01){
+    private static void printList(ArrayList<Integer> list) {
+        System.out.print("{ ");
+        for (int i = 0; i < list.size() - 1; i+=2) {
+            System.out.print(list.get(i)+", "+list.get(i+1));
+            if (i + 2 < list.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(" }");
+    }
+
+    private static void getChecking(ArrayList<Integer> list01){
         if (list01.size() == 0) {
             System.out.println(list01);
             System.out.println("empty");
             return;
         }
-
         if (list01.size() % 2 != 0) {
             int remove =  list01.size() - 1;
             list01.remove(remove);
         }
-
-
-        for (int i = 0; i < list01.size() - 1; i+=2) {
-            System.out.print("{"+list01.get(i)+", "+list01.get(i+1)+"} ,");
-        }
-
+        printList(list01);
         System.out.println();
-        print(list01);
+        conclusion(list01);
     }
-    private static void print(ArrayList<Integer> list01){
-        ArrayList<Integer> inAppropriate = new ArrayList<>();
+
+    private static void conclusion(ArrayList<Integer> list01){
         ArrayList<Integer> appropriate = new ArrayList<>();
         for (int i = 0; i < list01.size() - 1; i+=2) {
             if (list01.get(i) < list01.get(i+1)) {
                 System.out.print("<no> : {"+list01.get(i)+", "+list01.get(i+1)+"}, ");
                 System.out.println();
-                inAppropriate.add(list01.get(i));
-                inAppropriate.add(list01.get(i+1));
             }
             if (list01.get(i) >= list01.get(i+1)){
                 System.out.print("<yes> : {"+list01.get(i)+", "+list01.get(i+1)+"}, ");
@@ -56,11 +58,16 @@ public class Question3 {
                 appropriate.add(list01.get(i+1));
             }
         }
+        printConclusion(list01, appropriate);
+    }
+
+    private static void printConclusion(ArrayList<Integer> list01, ArrayList<Integer> appropriate){
         System.out.println("-----------------------------------------------------------------");
-        System.out.println(list01);
+        printList(list01);
         list01.clear();
         list01.addAll(appropriate);
-        System.out.println("after edition: "+list01);
+        System.out.print("The appropriate: ");
+        printList(list01);
         System.out.println("THE CANDIDATES ARE: ");
         for (int i = 0; i < list01.size() - 1; i+=2) {
             System.out.print("{"+list01.get(i)+", "+list01.get(i+1)+"} ");
