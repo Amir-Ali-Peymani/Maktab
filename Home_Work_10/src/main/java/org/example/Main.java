be2e2587482731201e3b26e8e22d891c6db9a294
Amir ali <amiralipeymanispacelove@gmail.com>
2023-06-29 08:19:00 +0330

    testing main

--- Home_Work_10/src/main/java/org/example/Main.java
+++ Home_Work_10/src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 
+import org.example.exception.CustomException;
 import org.example.question01.Question1_A;
 import org.example.question01.Question1_B;
-import org.example.question02.Question2_A;
-import org.example.question03.Question3;
 
 public class Main {
     public static void main(String[] args) {
+        CustomException customException = new CustomException();
 //        Question3.start();
 //        Question2_A.start();
 //        Question1_B.start();
-//        Question1_A.start();
+//        Question1_A.start(customException.giveInput("input"));
+//        Question1_B.start();
     }
 }
