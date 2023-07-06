69e68931ca824973bfb7892ff318e596efc831e5
Amir ali <amiralipeymanispacelove@gmail.com>
2023-07-06 10:17:52 +0330

    resource change to block

--- Home_Work_11/src/main/java/org/example/Q02/DeadLock.java
+++ Home_Work_11/src/main/java/org/example/Q02/DeadLock.java
@@ -8,12 +8,12 @@ public class DeadLock {
         Thread t1 = new Thread() {
             public void run() {
                 synchronized (block01) {
-                    System.out.println("Thread 1: locked resource 1");
+                    System.out.println("Thread 1: locked block 1");
 
                     try { Thread.sleep(100);} catch (Exception e) {}
 
                     synchronized (block02) {
-                        System.out.println("Thread 1: locked resource 2");
+                        System.out.println("Thread 1: locked block 2");
                     }
                 }
             }
@@ -22,12 +22,12 @@ public class DeadLock {
         Thread t2 = new Thread() {
             public void run() {
                 synchronized (block02) {
-                    System.out.println("Thread 2: locked resource 2");
+                    System.out.println("Thread 2: locked block 2");
 
                     try { Thread.sleep(100);} catch (Exception e) {}
 
                     synchronized (block01) {
-                        System.out.println("Thread 2: locked resource 1");
+                        System.out.println("Thread 2: locked block 1");
                     }
                 }
             }
