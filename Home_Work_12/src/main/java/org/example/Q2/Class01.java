f3bf07ddfe93f737d361b14512e017e7f4a6581d
Amir ali <amiralipeymanispacelove@gmail.com>
2023-07-20 02:29:38 +0330

    wrote the words in the List and sorted them with groupingBy

---
+++ Home_Work_12/src/main/java/org/example/Q2/Class01.java
@@ -0,0 +1,25 @@
+package org.example.Q2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Class01 {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("marry", "Kail", "amiral", "cat");
+
+        Map<Integer, List<String>> wordLength = words.stream()
+                .collect(Collectors.groupingBy(String::length));
+
+        words = wordLength.values().stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        System.out.println(words);
+
+
+    }
+}
