7cd0d5be58fa60349d96d06c0703b1240c6e0135
Amir ali <amiralipeymanispacelove@gmail.com>
2023-08-02 21:15:00 +0330

    made the b1, b2, b3 methods

--- Home_Work_13/src/main/java/ir/maktab/Main.java
+++ Home_Work_13/src/main/java/ir/maktab/Main.java
@@ -1,9 +1,40 @@
 package ir.maktab;
 
 
+import ir.maktab.mockdata.MockData;
+import ir.maktab.model.Person;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        List<Person> peopleList = MockData.getPeople();
 
+        List<Person> filteredPeopleList = filterAge(peopleList);
+
+        List<Person> orderingPeopleList = orderByUsername(peopleList);
+    }
+
+    public static List<Person> filterAge(List<Person> people) {
+        List<Person> overfifty = people.stream()
+                .filter(person -> person.getAge() >50)
+                .collect(Collectors.toList());
+        return overfifty;
+    }
+
+    public static List<Person> orderByUsername(List<Person> people) {
+        List<Person> orderedByUsername = people.stream()
+                .sorted(Comparator.comparing(Person::getUsername))
+                .toList();
+        return orderedByUsername;
+    }
+
+    public static List<Person> orderingByAgeByLastName(List<Person> people) {
+
+
+        return null;
     }
 }
