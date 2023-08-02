df4dfd452800b6f883b36515cddd6b4a9f7b0ac6
Amir ali <amiralipeymanispacelove@gmail.com>
2023-08-02 20:00:21 +0330

    added the lombok dependency

--- Home_Work_13/src/main/java/ir/maktab/model/Person.java
+++ Home_Work_13/src/main/java/ir/maktab/model/Person.java
@@ -1,5 +1,12 @@
 package ir.maktab.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class Person {
 
     private Integer id;
@@ -15,117 +22,5 @@ public class Person {
     private String ipv4;
     private String ipv6;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFavoriteColor() {
-        return favoriteColor;
-    }
-
-    public void setFavoriteColor(String favoriteColor) {
-        this.favoriteColor = favoriteColor;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getIpv4() {
-        return ipv4;
-    }
-
-    public void setIpv4(String ipv4) {
-        this.ipv4 = ipv4;
-    }
-
-    public String getIpv6() {
-        return ipv6;
-    }
-
-    public void setIpv6(String ipv6) {
-        this.ipv6 = ipv6;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", gender='" + gender + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", username='" + username + '\'' +
-                ", favoriteColor='" + favoriteColor + '\'' +
-                ", birthDate='" + birthDate + '\'' +
-                ", ipv4='" + ipv4 + '\'' +
-                ", ipv6='" + ipv6 + '\'' +
-                '}';
-    }
 }
