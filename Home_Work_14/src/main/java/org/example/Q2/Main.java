1a8c20d16c62bc49fe817d90050b8c8337683b74
Amir ali <amiralipeymanispacelove@gmail.com>
2023-08-10 04:38:47 +0330

    put some information about the person to the data base experiment

--- Home_Work_14/src/main/java/org/example/Q2/Main.java
+++ Home_Work_14/src/main/java/org/example/Q2/Main.java
@@ -1,4 +1,24 @@
 package org.example.Q2;
 
+import org.example.Q2.entity.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        Person person = new Person();
+        person.setName("amirali");
+        person.setLastName("Peymani");
+        person.setBirthDate(LocalDate.of(2000,3,2));
+        entityManager.persist(person);
+        entityManager.getTransaction().commit();
+    }
 }
