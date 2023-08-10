e964f0cfe1226ba5d3eed9c1400fd03519ebc30a
Amir ali <amiralipeymanispacelove@gmail.com>
2023-08-10 05:24:29 +0330

    testing PersonRepositoryImpl and PersonServiceImpl

--- Home_Work_14/src/main/java/org/example/Q2/Main.java
+++ Home_Work_14/src/main/java/org/example/Q2/Main.java
@@ -1,6 +1,8 @@
 package org.example.Q2;
 
 import org.example.Q2.entity.Person;
+import org.example.Q2.repository.impl.PersonRepositoryImpl;
+import org.example.Q2.service.impl.PersonServiceImpl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,13 +14,27 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
 
         EntityManager entityManager = emf.createEntityManager();
+//
+//        entityManager.getTransaction().begin();
+//        Person person = new Person();
+//        person.setName("amirali");
+//        person.setLastName("Peymani");
+//        person.setBirthDate(LocalDate.of(2000,3,2));
+//        entityManager.persist(person);
 
         entityManager.getTransaction().begin();
         Person person = new Person();
-        person.setName("amirali");
-        person.setLastName("Peymani");
-        person.setBirthDate(LocalDate.of(2000,3,2));
-        entityManager.persist(person);
+        PersonRepositoryImpl personRepository = new PersonRepositoryImpl(entityManager);
+        PersonServiceImpl service = new PersonServiceImpl(personRepository);
+        service.savePerson("amrirali", "peyia", LocalDate.of(2000,3,2));
+        entityManager.persist(service.savePerson("amrirali", "peyia", LocalDate.of(2000,3,2)));
+        entityManager.persist(service.updatePerson(1L, "amirreza", "peyia", LocalDate.of(2000,3,2)));
         entityManager.getTransaction().commit();
+        service.deletePerson(1L);
+        service.loadAllPersons();
+        service.getPersonById(1L);
+
+
+
     }
 }
