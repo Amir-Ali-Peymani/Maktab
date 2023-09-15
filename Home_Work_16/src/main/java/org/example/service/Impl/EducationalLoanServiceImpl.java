9983ad92dbcef5a7161bedd3cbda9557758c408d
amrialiPeymani <piliconwalys@gmail.com>
2023-09-15 16:41:20 +0330

    EducationalLoanServiceImpl.java body created

--- Home_Work_16/src/main/java/org/example/service/Impl/EducationalLoanServiceImpl.java
+++ Home_Work_16/src/main/java/org/example/service/Impl/EducationalLoanServiceImpl.java
@@ -1,4 +1,41 @@
 package org.example.service.Impl;
 
-public class EducationalLoanServiceImpl {
+import org.example.entity.EducationalLoan;
+import org.example.repository.EducationalLoanRepository;
+import org.example.service.EducationalLoanService;
+
+import java.util.List;
+
+public class EducationalLoanServiceImpl implements EducationalLoanService {
+
+    private final EducationalLoanRepository educationalLoanRepository;
+
+    public EducationalLoanServiceImpl(EducationalLoanRepository educationalLoanRepository){
+        this.educationalLoanRepository = educationalLoanRepository;
+    }
+
+    @Override
+    public void saveEducationalLoan(EducationalLoan educationalLoan) {
+        educationalLoanRepository.saveEducationalLoan(educationalLoan);
+    }
+
+    @Override
+    public EducationalLoan getEducationalById(Long id) {
+        return educationalLoanRepository.getEducationalLoanById(id);
+    }
+
+    @Override
+    public List<EducationalLoan> getAllEducationalLoans() {
+        return educationalLoanRepository.getAllEducationalLoan();
+    }
+
+    @Override
+    public void updateEducaionalLoan(EducationalLoan educationalLoan) {
+        educationalLoanRepository.updateEducationalLoan(educationalLoan);
+    }
+
+    @Override
+    public void deleteEducationalLoan(EducationalLoan educationalLoan) {
+        educationalLoanRepository.deleteEducationalLoan(educationalLoan);
+    }
 }
