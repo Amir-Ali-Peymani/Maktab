3e2a38de68de6fa3afa5bc735aedb7d55c07cf5d
amrialiPeymani <piliconwalys@gmail.com>
2023-09-15 15:58:51 +0330

    EducationalLoanService body created

--- Home_Work_16/src/main/java/org/example/service/EducationalLoanService.java
+++ Home_Work_16/src/main/java/org/example/service/EducationalLoanService.java
@@ -1,4 +1,19 @@
 package org.example.service;
 
+import org.example.entity.EducationalLoan;
+
+import java.util.List;
+
 public interface EducationalLoanService {
+
+    void saveEducationalLoan(EducationalLoan educationalLoan);
+
+    EducationalLoan getEducationalById(Long id);
+
+    List<EducationalLoan> getAllEducationalLoans();
+
+    void updateEducaionalLoan(EducationalLoan educationalLoan);
+
+    void deleteEducationalLoan(EducationalLoan educationalLoan);
+
 }
