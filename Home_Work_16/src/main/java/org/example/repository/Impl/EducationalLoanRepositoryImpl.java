e09e615a748720aaaa9c95dc70cbcc4ada256531
amrialiPeymani <piliconwalys@gmail.com>
2023-09-15 14:04:50 +0330

    EducationalLoanRepositoryImpl create the body

--- Home_Work_16/src/main/java/org/example/repository/Impl/EducationalLoanRepositoryImpl.java
+++ Home_Work_16/src/main/java/org/example/repository/Impl/EducationalLoanRepositoryImpl.java
@@ -1,4 +1,44 @@
 package org.example.repository.Impl;
 
-public class EducationalLoanRepositoryImpl {
+import org.example.base.BaseRepository;
+import org.example.entity.EducationalLoan;
+import org.example.repository.EducationalLoanRepository;
+
+import java.util.List;
+
+public class EducationalLoanRepositoryImpl extends BaseRepository implements EducationalLoanRepository {
+
+    @Override
+    public void saveEducationalLoan(EducationalLoan educationalLoan) {
+        em.getTransaction().begin();
+        em.persist(educationalLoan);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public EducationalLoan getEducationalLoanById(Long id) {
+        EducationalLoan educationalLoan = em.find(EducationalLoan.class, id);
+        return educationalLoan;
+    }
+
+    @Override
+    public List<EducationalLoan> getAllEducationalLoan() {
+        List<EducationalLoan> educationalLoans =
+                em.createQuery("SELECT e FROM EducationalLoan e", EducationalLoan.class).getResultList();
+        return educationalLoans;
+    }
+
+    @Override
+    public void updateEducationalLoan(EducationalLoan educationalLoan) {
+        em.getTransaction().begin();
+        em.merge(educationalLoan);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public void deleteEducationalLoan(EducationalLoan educationalLoan) {
+        em.getTransaction().begin();
+        em.remove(em.contains(educationalLoan) ? educationalLoan : em.merge(educationalLoan));
+        em.getTransaction().commit();
+    }
 }
