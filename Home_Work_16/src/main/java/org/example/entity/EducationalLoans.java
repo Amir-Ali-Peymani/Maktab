affeb96fa7eb24e9ed00b2964ade151e290da66b
amrialiPeymani <piliconwalys@gmail.com>
2023-09-15 13:55:11 +0330

    create educationalLoans and the body

---
+++ Home_Work_16/src/main/java/org/example/entity/EducationalLoans.java
@@ -0,0 +1,29 @@
+package org.example.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EducationalLoans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String paymentMethod;
+
+    private String price;
+
+    private String section;
+
+
+}
