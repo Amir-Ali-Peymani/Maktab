68663e09f48253e7b80e21c973c13c688061f619
amrialiPeymani <piliconwalys@gmail.com>
2023-10-03 16:18:53 +0330

    written the baseRepository class for connection to the hibernate database

---
+++ Home_Work_17/src/main/java/org/example/base/BaseRepository.java
@@ -0,0 +1,17 @@
+package org.example.base;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
+
+public class BaseRepository {
+    protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+    protected static EntityManager em = emf.createEntityManager();
+
+    protected static final Scanner scanner = new Scanner(System.in);
+
+}
