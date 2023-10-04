cec007eba725d9456f4958ce80a1ae8aa5929c24
amrialiPeymani <piliconwalys@gmail.com>
2023-10-05 00:43:16 +0330

    StudentCourseRating.java

--- Home_Work_17/src/main/java/org/example/Q2/entity/StudentCourseRating.java
+++ Home_Work_17/src/main/java/org/example/Q2/entity/StudentCourseRating.java
@@ -1,4 +1,34 @@
 package org.example.Q2.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentCourseRating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private LocalDateTime timeStamp;
+
+    private double rating;
+
+    private String comment;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
 }
