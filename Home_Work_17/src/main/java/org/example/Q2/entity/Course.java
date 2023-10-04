9f14962e9ed3570c157633e65cfd2ff0125f0725
amrialiPeymani <piliconwalys@gmail.com>
2023-10-05 00:41:29 +0330

    Course.java

--- Home_Work_17/src/main/java/org/example/Q2/entity/Course.java
+++ Home_Work_17/src/main/java/org/example/Q2/entity/Course.java
@@ -1,4 +1,30 @@
 package org.example.Q2.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String courseName;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @OneToOne(mappedBy =  "Course")
+    private StudentCourseRating studentCourseRating;
+
 }
